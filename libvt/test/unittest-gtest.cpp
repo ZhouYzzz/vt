732df470021254a8d59926c8b666445c238c3567
ZhouYzzz <zhouyz9608@gmail.com>
2017-09-17 13:48:33 +0800

    UnitTests setup

---
+++ libvt/test/unittest-gtest.cpp
@@ -0,0 +1,15 @@
+//
+//  unittest-gtest.cpp
+//  vt
+//
+//  Created by Yizhuang Zhou on 17/09/2017.
+//  Copyright © 2017 Yizhuang Zhou. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "gtest/gtest.h"
+
+TEST(GTEST, GTEST) {
+	EXPECT_EQ(0, 0);
+}
