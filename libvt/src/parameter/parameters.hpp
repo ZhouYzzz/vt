80f49d9befd5dcb1d533abc1af8384c5de3094ef
ZhouYzzz <zhouyz9608@gmail.com>
2017-09-21 11:12:05 +0800

    add parameter structure

---
+++ libvt/src/parameter/parameters.hpp
@@ -0,0 +1,20 @@
+//
+//  parameters.hpp
+//  vt
+//
+//  Created by Yizhuang Zhou on 18/09/2017.
+//  Copyright © 2017 Yizhuang Zhou. All rights reserved.
+//
+
+#ifndef parameters_hpp
+#define parameters_hpp
+
+#include <stdio.h>
+
+struct parameter {
+	bool visualization = true;
+	
+	float search_window_scale = 4;
+};
+
+#endif /* parameters_hpp */
