9c55ff8871456a15cbadfa15d6c512d01226e2f6
ZhouYzzz <zhouyz9608@gmail.com>
2017-09-23 14:02:00 +0800

    tracker interface

---
+++ libvt/src/tracker/tracker.cpp
@@ -0,0 +1,9 @@
+//
+//  tracker.cpp
+//  vt
+//
+//  Created by Yizhuang Zhou on 23/09/2017.
+//  Copyright © 2017 Yizhuang Zhou. All rights reserved.
+//
+
+#include "tracker.hpp"
---
+++ libvt/src/tracker/tracker.hpp
@@ -0,0 +1,14 @@
+//
+//  tracker.hpp
+//  vt
+//
+//  Created by Yizhuang Zhou on 23/09/2017.
+//  Copyright © 2017 Yizhuang Zhou. All rights reserved.
+//
+
+#ifndef tracker_hpp
+#define tracker_hpp
+
+#include <stdio.h>
+
+#endif /* tracker_hpp */
